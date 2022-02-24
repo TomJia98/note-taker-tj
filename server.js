becad8c00c1c95cbc832dd607160310f5291748f
Tom Jia <tomchristian1998@gmail.com>
2022-02-24 18:33:12 +0800

    added some code to the server.js to get the notes saving

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const path = require('path');
+const fs = require("fs");
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+
+    console.log("api/notes get req")
+    res.sendFile(path.join(__dirname, '/db/db.json'))
+});
+
+
+app.post('/api/notes', (req, res) => {
+    let newNote = req.body
+
+
+
+
+
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes = JSON.parse(data);
+  
+          // Add a new review
+          parsedNotes.push(newNote);
+  
+          // Write updated reviews back to the file
+          fs.writeFile(
+            'db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated notes!')
+          );
+        }
+      })
+
+
+console.log(typeof newNote)
+
+
+
+res.status(200).send(`User created!`);
+}
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
